22207bde3c55d92cc7660f6a7487ca14e9c24e4d
Marcel Féo <marcelfeo29@gmail.com>
2023-05-01 23:13:21 -0300

    Add exercícios da aula 2 (PRÁTICA)

---
+++ Aula02/Exercicios/ex01.rb
@@ -0,0 +1,6 @@
+puts "Me fale seu nome e sua idade que eu vou adivinhar seu nome e sua idade :)"
+puts "Qual seu nome? "
+nome = gets.chomp
+puts "Qual sua idade? "
+idade = gets.chomp.to_i
+puts "Voce é #{nome} e sua idade é #{idade} anos"
---
+++ Aula02/Exercicios/ex02.rb
@@ -0,0 +1,11 @@
+puts "Digite dois números e eu vou mostrar as operações entre eles."
+puts "Digite o primeiro número: "
+num1 = gets.chomp.to_i
+puts "Digite o segundo número: "
+num2 = gets.chomp.to_i
+
+puts "Aqui estão os resultados:"
+puts "#{num1} + #{num2} = #{num1 + num2}"
+puts "#{num1} - #{num2} = #{num1 - num2}"
+puts "#{num1} * #{num2} = #{num1 * num2}"
+puts "#{num1} / #{num2} = #{num1 / num2}"
